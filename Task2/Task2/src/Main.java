84aea56450d5704b7404a37da2fa54035a3b5e38
Marwan Walid <87389616+Marwan-alanani@users.noreply.github.com>
2023-11-23 15:29:30 +0200

    Add files via upload

---
+++ Task2/Task2/src/Main.java
@@ -0,0 +1,17 @@
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Generics l = new Generics();
+        System.out.println(l.Reverse(60002));
+        System.out.println(l.randomInt(8));
+        System.out.println(l.randomInt(20,10));
+        Integer []n = {1,2,3,4};
+        l.printData(n);
+        System.out.println(l.randomCharacter());
+
+    }
+
+
+}
