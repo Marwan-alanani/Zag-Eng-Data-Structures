9ed0d45b0d1d2d2ef9e84e8ecd316a25dd75daab
Marwan Walid <87389616+Marwan-alanani@users.noreply.github.com>
2023-11-24 16:03:32 +0200

    Add files via upload

--- Task2/Task2/src/Generics.java
+++ Task2/Task2/src/Generics.java
@@ -1,35 +1,38 @@
 
 
-public class  Generics {
-    public Integer Reverse(Integer no){
+public class Generics {
+    public static Integer reverse(Integer no) {
         char[] numbersArray = no.toString().toCharArray();
         String number = "";
-        for (int i = numbersArray.length-1;i>-1;i--){
-            number+=numbersArray[i];
+        for (int i = numbersArray.length - 1; i > -1; i--) {
+            number += numbersArray[i];
         }
 
         return Integer.parseInt(number);
     }
-    public int randomInt(Integer no){
-        double random = Math.random()*(no+1);
+
+    public static int randomInt(Integer no) {
+        double random = Math.random() * (no + 1);
         random = Math.floor(random);
         return (int) (random);
     }
-    public int randomInt(Integer max,Integer min){
-        if(min>max)
-        {
+
+    public static int randomInt(Integer max, Integer min) {
+        if (min > max) {
             System.out.println("Incorrect use of function.");
             return 1;
         }
-        return min + randomInt((max-min));
+        return min + randomInt((max - min));
     }
-    public Character  randomCharacter(){
-        return  (char) (randomInt((int) ('Z'),(int)('A')));
+
+    public static Character randomCharacter() {
+        return (char) (randomInt((int) ('Z'), (int) ('A')));
     }
-    public <T> void  printData(T[] data){
+
+    public static <T> void printData(T[] data) {
         for (T element : data) {
             System.out.print(element);
         }
-       System.out.println();
+        System.out.println();
     }
 }
--- Task2/Task2/src/Main.java
+++ Task2/Task2/src/Main.java
@@ -3,13 +3,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Generics l = new Generics();
-        System.out.println(l.Reverse(60002));
-        System.out.println(l.randomInt(8));
-        System.out.println(l.randomInt(20,10));
-        Integer []n = {1,2,3,4};
-        l.printData(n);
-        System.out.println(l.randomCharacter());
+        System.out.println(Generics.reverse(60002));
+        System.out.println(Generics.randomInt(8));
+        System.out.println(Generics.randomInt(20, 10));
+        Integer[] n = {1, 2, 3, 4};
+        Generics.printData(n);
+        System.out.println(Generics.randomCharacter());
 
     }
 
